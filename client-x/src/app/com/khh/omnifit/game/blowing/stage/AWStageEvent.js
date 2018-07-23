ffaffac635619b842103604560a48713c645eead
김현하 <khh@omnicns.com>
2018-07-23 15:29:37 +0900

    g

--- client-x/src/app/com/khh/omnifit/game/blowing/stage/AWStageEvent.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var AWStageEvent;
-(function (AWStageEvent) {
-    AWStageEvent["EVENT_CLOCK"] = "EVENT_CLOCK";
-    AWStageEvent["EVENT_CONCENTRATION"] = "EVENT_CONCENTRATION";
-    AWStageEvent["EVENT_ROOM_DETAIL"] = "EVENT_ROOM_DETAIL";
-})(AWStageEvent = exports.AWStageEvent || (exports.AWStageEvent = {}));
